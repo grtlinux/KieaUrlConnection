150e01c54dc47b0222a042c6b5a1d36d871b20db
grtlinux <grtlinux@gmail.com>
2022-03-03 18:25:20 +0900

    create 1st

---
+++ ver01/KieaUrlConnection-01/src/main/java/org/tain/KieaUrlConnection01Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaUrlConnection01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaUrlConnection01Application.class, args);
+	}
+
+}
